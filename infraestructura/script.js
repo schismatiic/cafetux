1182a5991418e5a9533e2c8fb6fe61db09d8aabe
schismatiic <leonardojofre2003@gmail.com>
2026-04-30 23:12:55 -0400

    Se renderiza el nombre de la página

--- infraestructura/script.js
+++ infraestructura/script.js
@@ -1,2 +1,36 @@
-const renderInicio = () => {};
+//   ===============================================================================================
+//   DOM elements
+const content = document.getElementById("content");
+const tux = document.getElementById("tux");
+const boton_inicio = document.getElementById("inicio");
+const boton_catalogo = document.getElementById("catalogo");
+const boton_nosotros = document.getElementById("nosotros");
+const titulo__pagina = document.getElementById("titulo__pagina");
+const botones_nav = document.querySelectorAll(".boton__nav");
+//   ===============================================================================================
+//   Variables
+let toggleTux = true;
+//   ===============================================================================================
+//   Render
+const renderInicio = () => {
+  //   ===============================================================================================
+  //   DOM elements
+  const inicio_container = document.createElement("div");
+};
+//   ===============================================================================================
+//   Listeners
+botones_nav.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    titulo__pagina.textContent = boton.textContent;
+  });
+});
+tux.addEventListener("click", () => {
+  if (toggleTux) {
+    toggleTux = false;
+    tux.src = "https://media.tenor.com/S61VCO73mOAAAAAj/linux-tux.gif";
+  } else {
+    toggleTux = true;
+    tux.src = "https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png";
+  }
+});
 renderInicio();
