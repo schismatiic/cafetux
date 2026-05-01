c1c60cf9399c01cad1ff61720b1952699af778a6
schismatiic <leonardojofre2003@gmail.com>
2026-04-30 23:45:32 -0400

    Comienzo del render por página

--- infraestructura/script.js
+++ infraestructura/script.js
@@ -13,15 +13,55 @@ let toggleTux = true;
 //   ===============================================================================================
 //   Render
 const renderInicio = () => {
+  content.innerHTML = "";
   //   ===============================================================================================
   //   DOM elements
   const inicio_container = document.createElement("div");
+  const inicio_img = document.createElement("img");
+  const inicio_p = document.createElement("p");
+  //   ===============================================================================================
+  //   Classname and styles
+  inicio_container.className = "inicio_container";
+  //   ===============================================================================================
+  //   Text content / src
+  inicio_img.src =
+    "https://larazacoffee.cl/cdn/shop/articles/v60.webp?v=1685481020";
+  inicio_p.textContent =
+    "Inspirados en la pasión de Momo por el café y las cafeteras, en Cafetux reunimos modelos modernos y funcionales para quienes disfrutan preparar un buen café en casa. Desde la Saeco A-50 hasta la Bialetti que es el típico café napolitano.";
+  inicio_container.appendChild(inicio_img);
+  inicio_container.appendChild(inicio_p);
+  content.appendChild(inicio_container);
+};
+const renderCatalogo = () => {
+  content.innerHTML = "";
+  //   ===============================================================================================
+  //   DOM elements
+  const inicio_container = document.createElement("div");
+  const inicio_img = document.createElement("img");
+  const inicio_p = document.createElement("p");
+  //   ===============================================================================================
+  //   Classname and styles
+  inicio_container.className = "inicio_container";
+  //   ===============================================================================================
+  //   Text content / src
+  inicio_img.src =
+    "https://larazacoffee.cl/cdn/shop/articles/v60.webp?v=1685481020";
+  inicio_p.textContent =
+    "Inspirados en la pasión de dsadsadasdasda por el café y las cafeteras, en Cafetux reunimos modelos modernos y funcionales para quienes disfrutan preparar un buen café en casa. Desde la Saeco A-50 hasta la Bialetti que es el típico café napolitano.";
+  inicio_container.appendChild(inicio_img);
+  inicio_container.appendChild(inicio_p);
+  content.appendChild(inicio_container);
 };
 //   ===============================================================================================
 //   Listeners
 botones_nav.forEach((boton) => {
   boton.addEventListener("click", () => {
     titulo__pagina.textContent = boton.textContent;
+    if (boton.textContent === "Inicio") {
+      renderInicio();
+    } else if (boton.textContent === "Catálogo") {
+      renderCatalogo();
+    }
   });
 });
 tux.addEventListener("click", () => {
